4935e45642df3453b9548f9208c4d1c0b2dbdc3d
tpsit <14529993@itisrossi.vi.it>
2026-02-07 11:55:51 +0100

    ffw

---
+++ Libro.cs
@@ -0,0 +1,44 @@
+//libro titolo, autore, anno, pagine
+public class Libro
+{
+    string titolo;
+    string autore;
+    int anno;
+    int pagine;
+    float prezzo;
+    // Metodi
+    /// <summary>
+    /// Costruttore di default
+    /// </summary>
+    public Libro()
+    {
+    titolo="Specifica un titolo";
+    autore="Scegli un autore";
+    anno=0;
+    pagine=0;
+    prezzo=0.0f;
+    }
+    /// <summary>
+    /// Costruttore con parametri
+    /// </summary>
+    public Libro(string titolo, string autore, int anno, int pagine,float prezzo)
+    {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.anno = anno;
+        this.pagine = pagine;
+        this.prezzo = prezzo;
+    }
+    /// <summary>
+    /// Ritorna il titolo del libro
+    /// </summary>
+    /// <returns>Il titolo</returns>
+    public string getTitolo()
+    {
+        return this.titolo;
+    }
+    public override string ToString()
+    {
+        return $"'{this.titolo}' di {this.autore} ({this.anno}) - {this.pagine} pagine - {this.prezzo} euro";
+};
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string nome;
+nome="lucia";
+
+/*
+Libro:classe
+libro():costruttore di default(==senza parametri) della classe libro
+uno: istanza della classe libro noto come OGGETTO cioè una VARIABILE DI TIPO LIBRO (CLASSE)
+*/
+
+Libro  uno=new Libro();// DICHIARAZIONE E ASSEGNAMENTO tramite COSTRUTTORE
+Libro due=new Libro("Il ritorno del re ","Tolkier",1954,1300,17.10f);
+
+Console.WriteLine($"Il libro preferito da {nome} è {due.getTitolo()}");
