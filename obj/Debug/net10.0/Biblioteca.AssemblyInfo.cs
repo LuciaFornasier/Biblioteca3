9143b33fe73cdfafe37a08dfecfc1c9f0808f64f
tpsit <14529993@itisrossi.vi.it>
2026-02-12 08:08:23 +0100

    f#

--- obj/Debug/net10.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net10.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2073fd60b5c29a2edb1267697a7b10d6004f368b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e4619efc5318541bccc8db26a2781894473063")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
