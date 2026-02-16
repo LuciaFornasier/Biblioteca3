9ac886289d2b45e54a025888925819eb31755389
computercasa <1452993@itisrossi.vi.it>
2026-02-16 11:05:35 +0100

    h

--- Autore.cs
+++ Autore.cs
@@ -8,26 +8,24 @@ public class Autore {
         nascita=nascita.Minvalue
     }
     
-    public Autore(string nome; string cognome; DateOnly nascita){
+    public Autore(string nome, string cognome, DateOnly nascita){
         this.nome=nome;
         this.cognome=cognome;
         this.nascita=nascita;
-        this.libro=new list<Libro>;
+        this.libri=new List<Libro>;
     
     }
     
-    using(StreamReader reader =new StreamReader(filePath)){
         private list<Libro> libri;
-    }
-    using(StreamReader writer = ew StreamWriter(filePath)){
+    
         public int  Aggiungi(Libro libronuovo)
         {
             this.libri.Add(libronuovo);
             return this.libri.Count;
         }
-    }
 
 }
+
 
 
 
