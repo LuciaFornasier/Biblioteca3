95923d312f2ef373df1249146e98ac6ffd85dc6f
computercasa <1452993@itisrossi.vi.it>
2026-03-04 18:35:55 +0100

    m

--- obj/Debug/net10.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net10.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb5ccf0aeed3555e6d39ada83cc29d97382f7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d625b649bc3282f84fe1362c5354b658b377ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
