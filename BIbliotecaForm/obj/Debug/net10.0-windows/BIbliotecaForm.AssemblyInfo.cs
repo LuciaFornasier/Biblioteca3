98af2f844a8ebdf6e0eba3ca884e0cf89d8a5b62
tpsit <14529993@itisrossi.vi.it>
2026-04-18 12:00:45 +0200

    x

--- BIbliotecaForm/obj/Debug/net10.0-windows/BIbliotecaForm.AssemblyInfo.cs
+++ BIbliotecaForm/obj/Debug/net10.0-windows/BIbliotecaForm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIbliotecaForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ca1fc013e47a6c5edc69a86d124d0bdc430b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6977541591d6cf035d1606115d031920641652")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIbliotecaForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIbliotecaForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
